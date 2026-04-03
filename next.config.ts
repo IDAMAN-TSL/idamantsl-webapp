3c37061044cc47d2060a9e7d01bb2e928fe42e8e
ojan <muhammadfauzan292004@gmail.com>
2026-04-03 15:16:01 +0700

    fix: make basPath ucon appluy cleanly

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/idamantsl-webapp',
+  basePath: process.env.NODE_ENV === 'production' ? '/idamantsl-webapp' : '',
   images: {
     unoptimized: true,
     qualities: [25, 50, 75, 100],
